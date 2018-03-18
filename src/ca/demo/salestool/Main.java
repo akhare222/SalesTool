354ca0541afd59b20e6f3164bfe8746dc8629428
Aditi Saxena <asaxena22@outlook.com>
2018-03-18 10:40:08 -0400

    initial commit

---
+++ src/ca/demo/salestool/Main.java
@@ -0,0 +1,14 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData data = new SalesData();
+		data.display();
+		System.out.println("This is statement 1");
+		System.out.println("This is statement 2");
+
+	}
+
+}
---
+++ src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,14 @@
+package ca.demo.salestool;
+
+public class SalesData {
+	
+	public void display(){
+		display1();
+		System.out.println("This is the output from Sales Data");
+	}
+	
+	public void display1(){
+		System.out.println("This is another data from SalesData class");
+	}
+
+}
